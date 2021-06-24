2ae1d669bed4a9cd1bbd536c5c63a718fd9a3fe8
Kausinho <kausinhobrunow@gmail.com>
2021-06-24 19:50:27 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet" />
+	<script src='https://cdn.scaledrone.com/scaledrone.min.js' type='text/javascript'></script>
+	<title>Projeto Vídeo Chat</title>
+</head>
+<body>
+	
+	<div class="title">Bem-Vindo ao chat em tempo real!</div>
+
+	<video id="localVideo" autoplay muted></video>
+	<video id="remoteVideo" autoplay></video>
+
+	<script src="js/file.js"></script>
+</body>
+</html>
