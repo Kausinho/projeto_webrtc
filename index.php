5b9158a2f80d23aac7a8cfa7d63832a8221480d6
Kausinho <kausinhobrunow@gmail.com>
2021-07-02 09:35:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
 	<link href="css/style.css" rel="stylesheet" />
-	<script src='https://cdn.scaledrone.com/scaledrone.min.js' type='text/javascript'></script>
+	
 	<title>Projeto Vídeo Chat</title>
 </head>
 <body>
@@ -16,6 +16,8 @@
 	<video id="localVideo" autoplay muted></video>
 	<video id="remoteVideo" autoplay></video>
 
+	<script src='https://cdn.scaledrone.com/scaledrone.min.js' type='text/javascript'></script>
 	<script src="js/file.js"></script>
+	
 </body>
 </html>
